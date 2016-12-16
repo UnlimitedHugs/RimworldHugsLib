146c3fc5787cc487b5d0a4412f2882d27ee4c144
FluffierThanThou <fluffy.l2032@gmail.com>
2016-12-16 20:53:21 +0100

    Make source uglier

--- Source/DetourByAttribute/Attribute.cs
+++ Source/DetourByAttribute/Attribute.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Security.Policy;
-using System.Text;
 
-namespace HugsLib.DetourByAttribute
-{
+namespace HugsLib.DetourByAttribute {
     [Flags]
-    public enum DetourProperty
-    {
+    public enum DetourProperty {
         None = 0,
         Getter = 1,
         Setter = 2,
@@ -17,60 +11,53 @@ namespace HugsLib.DetourByAttribute
     }
 
     [AttributeUsage( AttributeTargets.Method, AllowMultiple = true, Inherited = false )]
-    public class DetourMethodAttribute : Attribute
-    {
+    public class DetourMethodAttribute : Attribute {
+        public MethodInfo sourceMethodInfo;
+        public string sourceMethodName;
         // store references needed for detours
         public Type sourceType;
-        public string sourceMethodName;
-        public MethodInfo sourceMethodInfo;
-
-        // check how this attribute was created
-        public bool WasSetByMethodInfo
-        {
-            get { return sourceMethodInfo != null; }
-        } 
 
         // disable default constructor
         private DetourMethodAttribute() { }
 
         // get by name
-        public DetourMethodAttribute( Type sourceType, string sourceMethodName )
-        {
+        public DetourMethodAttribute( Type sourceType, string sourceMethodName ) {
             this.sourceType = sourceType;
             this.sourceMethodName = sourceMethodName;
         }
 
         // get by methodInfo
-        public DetourMethodAttribute( MethodInfo methodInfo )
-        {
+        public DetourMethodAttribute( MethodInfo methodInfo ) {
             sourceType = methodInfo.DeclaringType;
             sourceMethodInfo = methodInfo;
+        }
+
+        // check how this attribute was created
+        public bool WasSetByMethodInfo {
+            get { return sourceMethodInfo != null; }
         }
     }
 
 
     [AttributeUsage( AttributeTargets.Property, AllowMultiple = true, Inherited = false )]
-    public class DetourPropertyAttribute : Attribute
-    {
+    public class DetourPropertyAttribute : Attribute {
+        public DetourProperty detourProperty;
         // store references needed for detours
         public PropertyInfo sourcePropertyInfo;
-        public DetourProperty detourProperty;
 
         // disable default constructor
         private DetourPropertyAttribute() { }
 
         // get by name
         public DetourPropertyAttribute( Type sourceType, string sourcePropertyName,
-                                        DetourProperty detourProperty = DetourProperty.Both )
-        {
+                                        DetourProperty detourProperty = DetourProperty.Both ) {
             sourcePropertyInfo = sourceType.GetProperty( sourcePropertyName, Helpers.AllBindingFlags );
             this.detourProperty = detourProperty;
         }
 
         // get by propertyInfo
         public DetourPropertyAttribute( PropertyInfo sourcePropertyInfo,
-                                        DetourProperty detourProperty = DetourProperty.Both )
-        {
+                                        DetourProperty detourProperty = DetourProperty.Both ) {
             this.sourcePropertyInfo = sourcePropertyInfo;
             this.detourProperty = detourProperty;
         }
