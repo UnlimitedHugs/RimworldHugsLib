80d7cf03f922442c8d1c4dfdb77b568fe49bb391
UnlimitedHugs <unlimited.hugs@gmail.com>
2020-02-27 19:30:45 +0400

    Added PackageIds to log publisher

--- Source/Logs/LogPublisher.cs
+++ Source/Logs/LogPublisher.cs
@@ -382,7 +382,7 @@ namespace HugsLib.Logs {
 			var builder = new StringBuilder();
 			builder.Append("Loaded mods:\n");
 			foreach (var modContentPack in LoadedModManager.RunningMods) {
-				builder.Append(modContentPack.Name);
+				builder.AppendFormat("{0}({1})", modContentPack.Name, modContentPack.PackageIdPlayerFacing);
 				var versionFile = VersionFile.TryParseVersionFile(modContentPack);
 				if (versionFile != null && versionFile.OverrideVersion != null) {
 					builder.AppendFormat("[ov:{0}]: ", versionFile.OverrideVersion);
