12e480c6b44e0f6e0e4a2071bacb28c9fe886e16
UnlimitedHugs <unlimited.hugs@gmail.com>
2020-04-24 15:01:34 +0400

    Consume key press events for HugsLib shortcuts

--- Source/Core/KeyBindingHandler.cs
+++ Source/Core/KeyBindingHandler.cs
@@ -10,24 +10,33 @@ namespace HugsLib.Core {
 	internal static class KeyBindingHandler {
 		public static void OnGUI() {
 			if (Event.current.type != EventType.KeyDown) return;
+			var useEvent = false;
 			if (HugsLibKeyBindings.PublishLogs.JustPressed && HugsLibUtility.ControlIsHeld) {
 				if (HugsLibUtility.AltIsHeld) {
 					HugsLibController.Instance.LogUploader.CopyToClipboard();
 				} else {
 					HugsLibController.Instance.LogUploader.ShowPublishPrompt();
 				}
+				useEvent = true;
 			}
 			if (HugsLibKeyBindings.OpenLogFile.JustPressed) {
 				ShellOpenLog.Execute();
+				useEvent = true;
 			}
 			if (HugsLibKeyBindings.RestartRimworld.JustPressed) {
 				GenCommandLine.Restart();
+				useEvent = true;
 			}
 			if (HugsLibKeyBindings.HLOpenModSettings.JustPressed) {
 				HugsLibUtility.OpenModSettingsDialog();
+				useEvent = true;
 			}
 			if (HugsLibKeyBindings.HLOpenUpdateNews.JustPressed) {
 				HugsLibController.Instance.UpdateFeatures.TryShowDialog(true);
+				useEvent = true;
+			}
+			if (useEvent) {
+				Event.current.Use();
 			}
 		}
 	}
