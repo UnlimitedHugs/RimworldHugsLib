69d55ee32ed27fa15597f33a4b784d95c42a3079
UnlimitedHugs <unlimited.hugs@gmail.com>
2020-05-21 16:46:04 +0400

    Reverted OnGUI hook timing change

--- Source/Patches/Root_OnGUI_Patch.cs
+++ Source/Patches/Root_OnGUI_Patch.cs
@@ -4,19 +4,16 @@ using Verse;
 
 namespace HugsLib.Patches {
 	/// <summary>
-	/// Hooks into the flow of the vanilla MonoBehavior.OnGUI()
-	/// This allows to take advantage of automatic UI scaling (called after <see cref="UI.ApplyUIScale"/>)
-	/// and prevents GUI updates during a loading screen.
+	/// Hooks into the flow of the vanilla MonoBehavior.OnGUI().
+	/// Unlike the <see cref="UIRoot_OnGUI_Patch"/> patch, this hook also
+	/// allows to receive OnGUI events during loading screens. 
 	/// </summary>
 	[HarmonyPatch(typeof(Root))]
 	[HarmonyPatch(nameof(Root.OnGUI))]
 	[HarmonyPatch(new Type[0])]
 	internal static class Root_OnGUI_Patch {
 		[HarmonyPostfix]
-		private static void OnGUIHook() {
-			if (!LongEventHandler.ShouldWaitForEvent) {
-				HugsLibController.Instance.OnGUI();
-			}
+		private static void OnGUIHookUnfiltered() {
 			HugsLibController.Instance.OnGUIUnfiltered();
 		}
 	}
---
+++ Source/Patches/UIRoot_OnGUI_Patch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+
+namespace HugsLib.Patches {
+	/// <summary>
+	/// Hooks into the flow of the vanilla MonoBehavior.OnGUI()
+	/// This allows to take advantage of automatic UI scaling and prevents GUI updates during a loading screen.
+	/// </summary>
+	[HarmonyPatch(typeof(UIRoot))]
+	[HarmonyPatch("UIRootOnGUI")]
+	[HarmonyPatch(new Type[0])]
+	internal static class UIRoot_OnGUI_Patch {
+		[HarmonyPostfix]
+		private static void OnGUIHook() {
+			HugsLibController.Instance.OnGUI();
+		}
+	}
+}
