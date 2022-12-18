6f36d83c0a4ea51624cb65d73d08a0fc043035db
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-12-18 19:53:57 +0000

    chore: cleanup

--- Source/Settings/OptionsDialogExtensions.cs
+++ Source/Settings/OptionsDialogExtensions.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using HugsLib.Utils;
 using JetBrains.Annotations;
 using RimWorld;
 using Verse;
