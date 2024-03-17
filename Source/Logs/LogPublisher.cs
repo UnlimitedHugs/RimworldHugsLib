b93a09bef157f87a94ea5f7c1f5f893a832ef104
UnlimitedHugs <unlimitedhugs@protonmail.com>
2024-03-17 21:41:07 +0400

    Replace Gist publishing token

--- Source/Logs/LogPublisher.cs
+++ Source/Logs/LogPublisher.cs
@@ -34,7 +34,7 @@ namespace HugsLib.Logs {
 #else
 			false;
 #endif
-		private readonly string GitHubAuthToken = "6b69be56e8d8eaf678377c992a3d0c9b6da917e0".Reverse().Join(""); // GitHub will revoke any tokens committed
+		private readonly string GitHubAuthToken = "OptFd1QFKR5OJH0l9JzW8BIzFSLff006H7Hh_phg".Reverse().Join(""); // GitHub will revoke any tokens committed
 		private readonly Regex UploadResponseUrlMatch = new Regex("\"html_url\":\"(https://gist\\.github\\.com/\\w+)\"");
 
 		public enum PublisherStatus {
